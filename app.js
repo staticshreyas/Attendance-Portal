0b7db2f4ee75733b6c67661117d96399d96635f3
Shreyas <shreyas.mm@somaiya.edu>
2021-02-25 15:21:30 +0530

    ifEquals helper added

--- app.js
+++ app.js
@@ -40,6 +40,9 @@ const hbs=expressHbs.create({
          items.push(options.fn(context[i]));
       }
       return items;
+    },
+    ifEquals: function(arg1, arg2, options) {
+      return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
     }
   }
 })
