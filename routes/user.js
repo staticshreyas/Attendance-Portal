ee0ffb42afc7d64d05d93805c319499e9fb0feaa
Sakshi107 <46996938+Sakshi107@users.noreply.github.com>
2021-03-24 22:16:11 +0530

    Few UI fixes
    Signed-off-by: Sakshi107 <46996938+Sakshi107@users.noreply.github.com>

--- routes/user.js
+++ routes/user.js
@@ -121,7 +121,7 @@ router.get('/dashboard', isLoggedIn, function (req, res, next) {
         var b = classes[i].studentDetails[max]
         if (b) {
           if (parseFloat(b.percent) != 0) {
-            topAttPerStuPerClass.push({ className: classes[i].name, studentName: b.name, studentCounts: b.counts, studentPercent: b.percent.toString() })
+            topAttPerStuPerClass.push({ className: classes[i].name, studentName: b.name, studentCounts: b.counts, classCounts: classes[i].totLec,studentPercent: b.percent.toString() })
           }
         }
       }
