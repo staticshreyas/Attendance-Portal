9e232f29a6034c55ce5e294a439beb7a06e0709a
Shreyas <shreyas.mm@somaiya.edu>
2021-03-25 22:41:27 +0530

    Few bug fixes

--- api/api.js
+++ api/api.js
@@ -254,6 +254,8 @@ async function removeStudent(classId, studentId) {
 
     var name = student.name;
 
+    var records = await recordModel.deleteMany({ "data.Name": name })
+
     var del = await classModel.findOneAndUpdate({ _id: classId }, { $pull: { "students": studentId } }, { new: true })
 
 }
