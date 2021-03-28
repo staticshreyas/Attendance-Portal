b9f9d072ed8857c78271df603561a8c3983e09dc
Sakshi107 <46996938+Sakshi107@users.noreply.github.com>
2021-03-28 16:17:55 +0530

    working on join class with code
    Signed-off-by: Sakshi107 <46996938+Sakshi107@users.noreply.github.com>

--- models/class.js
+++ models/class.js
@@ -1,5 +1,21 @@
 const mongoose = require("mongoose");
 
+let alphabets = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+function generateCode() {
+  code=''
+  for(i=0;i<8;i++){
+    r=Math.floor(Math.random() * 10);
+    if(r<6){
+      code=code+alphabets[Math.floor(Math.random() * alphabets.length)]
+    }
+    else{
+      code=code+Math.floor(Math.random() * 10);
+    }
+  }
+  return code;
+}
+
 const classSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -27,6 +43,11 @@ const classSchema = new mongoose.Schema({
     required: false,
     default:0
   },
+  classCode:{
+    type:String,
+    required:false,
+    default:generateCode()
+  }
 });
 
 module.exports = new mongoose.model("Class", classSchema);
