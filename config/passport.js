22668edd8ef77952e4f9b76940025dfd80844d97
Shreyas <shreyas.mm@somaiya.edu>
2021-04-11 12:58:25 +0530

    Few UI changes

--- config/passport.js
+++ config/passport.js
@@ -84,6 +84,14 @@ passport.use('local-register', new LocalStrategy({
             messages.push("Please check the tickbox")
             return done(null, false, req.flash('error', messages));
         }
+        else if (year == "Year") {
+            messages.push("Please select a year")
+            return done(null, false, req.flash('error', messages));
+        }
+        else if (classs == "Class") {
+            messages.push("Please select a class")
+            return done(null, false, req.flash('error', messages));
+        }
         else {
             User.findOne({ 'email': email }, function (err, user) {
                 if (err) {
