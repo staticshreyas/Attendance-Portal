a0aa8de11ae85a0dabc1fe3499ff6b890e68ff10
akshit6 <akshit.gs@somaiya.edu>
2021-04-30 12:28:45 +0530

    copy code with toolip and  without redirecting

--- public/assets/js/classDetails.js
+++ public/assets/js/classDetails.js
@@ -19,4 +19,15 @@ $(document).ready(function () {
         send_android_path(this.value)
     });
 
+    $('.copy-code').on('click',function(){
+        $('<textarea>').val($(this).data("code")).appendTo('body').select();
+        document.execCommand('copy');
+        $('textarea').remove();
+        $(this).attr('data-original-title', "Copied: "+ $(this).data("code")).tooltip('show');
+    });
+
+    $('.copy-code').on('mouseleave',function(){
+        $(this).attr('data-original-title', "Copy to clipboard");
+    });
+
 });
